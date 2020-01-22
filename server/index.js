976a84d1726aafe0fc04f5f7a3da1bfe540a5df4
Robert Wilson <robert@theperfectfuel.com>
2020-01-22 00:57:34 -1000

    adds vue front end

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const students = require('./router/api/students.js');
 
